860caebdb3bf90b7a2b7ef011317608561a6cd5a
Dylan Ellison <dylancellison@gmail.com>
2017-07-17 13:23:52 -0700

    express server

---
+++ chilldyl.js
@@ -0,0 +1,20 @@
+var port = 44444;
+var greet = require('./public/greet')
+var app = require('express')();
+
+app.use(require('compression')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended:true }));
+/*app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));*/
+app.use(require('express').static('public'));
+
+app.get('/', function (req, res) {
+  res.send('nawascript');
+});
+
+greet.english()
+greet.spanish()
+
+require('http').createServer(app).listen(port,function(){
+  console.log('server is running you beeezy, on port', port)
+});
